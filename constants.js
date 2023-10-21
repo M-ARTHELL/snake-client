b8ac860f470e06b9e52283974f7b0371eb2c9423
M-ARTHELL <makoarthell@proton.me>
2023-10-21 20:32:55 -0300

    moved connections up for easier access, added name and instructions

--- constants.js
+++ constants.js
@@ -1,25 +1,34 @@
-// movement
-const MOVEMENT = {
-  w: 'Move: up',
-  a: 'Move: left',
-  s: 'Move: down',
-  d: 'Move: right'
-}
+// player name
+const NAME = 'Name: MAK';
 
-// messages
+// connection settings
+const IP = 'localhost';
+const PORT = 50541;
+
+// movement controls
+const MOVEMENT = {
+  'w': 'Move: up',
+  'a': 'Move: left',
+  's': 'Move: down',
+  'd': 'Move: right'
+};
+
+// message controls
 const MESSAGES = {
   '1': 'Say: hello',
   '2': 'Say: gg',
   '3': 'Say: bye'
 };
 
-// connection
-const IP = '::';
-const PORT = 50541;
+// instructions
+const HELP = '\nWelcome to Snake!\nThe goal of the game is to get as many points as you can by collecting the dots without running into your tail or the wall.\n\nControls:\n[MOVEMENT] W: Up, A: Left, S: Down, D: Right\n[MESSAGES] 1: "hello", 2: "gg", 3: "bye"\n\nPress CTRL + C to QUIT\n';
+
 
 module.exports = {
   IP,
   PORT,
   MOVEMENT,
-  MESSAGES
+  MESSAGES,
+  HELP,
+  NAME
 };
